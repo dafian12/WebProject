4feefb1ce05b273fcbc3150878aa7be282ac0444
dafian12 <akbardavian338@gmail.com>
2025-03-31 04:47:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ async function setupCamera() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true });
         videoElement.srcObject = stream;
-        await new Promise(resolve => videoElement.onloadedmetadata = resolve);
         videoElement.play();
         statusDiv.textContent = "Kamera berhasil terhubung.";
     } catch (error) {
@@ -20,7 +19,7 @@ async function setupCamera() {
 setupCamera();
 
 const hands = new Hands({
-    locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.4.1646424915/${file}`
+    locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
 });
 
 hands.setOptions({
@@ -32,8 +31,7 @@ hands.setOptions({
 
 hands.onResults(results => {
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
-
+    
     if (results.multiHandLandmarks && results.multiHandLandmarks.length > 0) {
         const landmarks = results.multiHandLandmarks[0];
         const indexFinger = landmarks[8]; 
